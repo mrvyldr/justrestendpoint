05440873b29da673a73503e997825a264cf49fc1
merve yıldır <mrvyldr@gmail.com>
2020-10-13 10:20:30 +0300

    Servlet is created

--- src/main/java/com/veniture/plugins/tutorial/servlet/MyServlet.java
+++ src/main/java/com/veniture/plugins/tutorial/servlet/MyServlet.java
@@ -10,6 +10,8 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("This is servlet");
+        resp.getWriter().close();
     }
 }
